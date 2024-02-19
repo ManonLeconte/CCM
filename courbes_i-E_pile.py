c108ce5dc0a51e871383308e4de14b0e15a653e8
ManonLeconte <110387764+ManonLeconte@users.noreply.github.com>
2024-02-19 11:55:31 +0100

    courbes_i-E_pile.py

--- courbes_i-E_pile.py
+++ courbes_i-E_pile.py
@@ -3,7 +3,14 @@ import matplotlib.pyplot as plt
 
 """
 Tracé des courbes intensité-potentiel pour la pile Daniell
+Manon Leconte
 """
+
+#%%Couleurs
+
+bleuvert=(0,.5,.5)
+violet=(.5,0,.5)
+jaunefonce=(.5,.5,0)
 
 #%% Définition des constantes
 
@@ -75,31 +82,48 @@ i_cat3=i_charge(i0,E3,E_cat)
 
 #%% Tracé du graphique
 
-plt.grid()
 plt.title("Courbes intensité-potentiel pour la pile Daniell")
 plt.xlabel("Potentiel E (V)")
 plt.ylabel("Intensité du courant i (A)")
 plt.xlim(-1.5,0.75)
 plt.ylim(-0.8,1)
-plt.text(E_an,-0.1,'$E_{an}$ = -0,82 V', horizontalalignment = 'center', verticalalignment = 'center')
-plt.text(E_cat,-0.1,'$E_{cat}$ = 0,28 V', horizontalalignment = 'center', verticalalignment = 'center')
-plt.text(-0.68,0.5,'Zn -> Zn$^{2+}$', horizontalalignment = 'center', verticalalignment = 'center')
+#Anode
+plt.text(E_an+.1,-0.1,'$E_{an,th}$', horizontalalignment = 'center', verticalalignment = 'center')
+plt.arrow(E_an,-.025,dx=0,dy=.05,ec='k')
+plt.text(-0.68,0.5,'Zn $\longrightarrow$ Zn$^{2+}$', horizontalalignment = 'center', verticalalignment = 'center')
+plt.arrow(x=E_an,y=.05,dx=.05,dy=0,head_width=.03,head_length=.05,fc=jaunefonce,ec=jaunefonce)
+plt.text(-.65,.05,'$\eta_a$', horizontalalignment = 'center', verticalalignment = 'center',color=jaunefonce)
+#Cathode
+plt.text(E_cat+.1,-0.1,'$E_{cat,th}$', horizontalalignment = 'center', verticalalignment = 'center')
+plt.arrow(E_cat,-.025,dx=0,dy=.05,ec='k')
+plt.arrow(x=E_cat,y=.05,dx=-.1,dy=0,head_width=.03,head_length=.05,fc=jaunefonce,ec=jaunefonce)
+plt.text(E_cat-.2,.05,'$\eta_c$', horizontalalignment = 'center', verticalalignment = 'center',color=jaunefonce)
 plt.text(0.2,-0.3,'Cu $\leftarrow$ Cu$^{2+}$', horizontalalignment = 'center', verticalalignment = 'center')
 plt.text(-1.13,-0.7,'H$_2\leftarrow$ H$^{+}$', horizontalalignment = 'center', verticalalignment = 'center')
+#Tension délivrée
+plt.arrow(x=-.7,y=.25,dx=.75,dy=0,head_width=.03,head_length=.05,fc=jaunefonce,ec=jaunefonce)
+plt.arrow(x=.1,y=.25,dx=-.75,dy=0,head_width=.03,head_length=.05,fc=jaunefonce,ec=jaunefonce)
+plt.text(-.5,.3,'$E_{cat}-E_{an}$',color=jaunefonce)
+#Courant
+plt.arrow(-.025,icl,dx=0.05,dy=0,ec='k')
+plt.arrow(-.025,-icl,dx=0.05,dy=0,ec='k')
+plt.text(.15,-icl,'$i_a=i$', horizontalalignment = 'center', verticalalignment = 'center')
 
-#Axe des abscisses
+
+#Axes
 plt.plot(E,abscisses,color='k')
+plt.axvline(x=0,color='k')
 
 #Pour l'anode :
-plt.plot(E0,i_an0,'g--')
-plt.plot(E1,i_an1,'g--')
-plt.plot(E2,i_an2,color="g",label="à l'anode de zinc")
+plt.plot(E0,i_an0,color=violet,ls='--')
+plt.plot(E1,i_an1,color=violet,ls='--')
+plt.plot(E2,i_an2,color=violet,label="à l'anode de zinc")
 
 #Pour la cathode :
-plt.plot(E0,i_cat0,'b',label="à la cathode de cuivre")
-plt.plot(E1,i_cat1,'b')
-plt.plot(E2,i_cat2,'b')
-plt.plot(E3,i_cat3,"b--")
+plt.plot(E0,i_cat0,color=bleuvert,label="à la cathode de cuivre")
+plt.plot(E1,i_cat1,color=bleuvert)
+plt.plot(E2,i_cat2,color=bleuvert)
+plt.plot(E3,i_cat3,color=bleuvert,ls='--')
 
 plt.legend()
 plt.show()
